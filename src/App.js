6231dc124bd49f00f21e5e3ba02dacc16facffdb
csttn <csttn.dev@gmail.com>
2020-01-16 12:40:43 -0300

    expensify

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+   return (
+      <div className="App">
+       <h1>puta q pariu caralhovbnvbn fgdfg de merda</h1>
+      </div>
+   )
+}
+
+export default App
