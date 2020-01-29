15efc73c25d0a71ea29095692d05bcbbf92bc8dd
csttn <csttn.dev@gmail.com>
2020-01-28 21:19:07 -0300

    test

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Water  haha bill", amount: 4500 }));
 store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
 store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
